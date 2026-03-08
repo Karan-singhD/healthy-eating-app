a0502252f690027276571055a1743712a0bd5ce9
Gurkaran Singh <gurkarandyalsingh@gmail.com>
2026-03-09 02:40:41 +1100

    MainActivity update and FruitScreen integration

--- app/src/main/java/com/example/healthyeatingapp/MainActivity.kt
+++ app/src/main/java/com/example/healthyeatingapp/MainActivity.kt
@@ -3,13 +3,18 @@ package com.example.healthyeatingapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.healthyeatingapp.ui.FruitScreen
 import com.example.healthyeatingapp.ui.theme.HealthyEatingAppTheme
+import com.example.healthyeatingapp.viewmodel.FruitViewModel
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             HealthyEatingAppTheme {
+                val fruitViewModel: FruitViewModel = viewModel()
+                FruitScreen(viewModel = fruitViewModel)
             }
         }
     }
