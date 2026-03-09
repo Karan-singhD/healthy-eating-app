c9f14232070d0a908281879c03af15d5bdf6ca07
Gurkaran Singh <gurkarandyalsingh@gmail.com>
2026-03-09 11:12:52 +1100

    Documentation  added to code files

--- app/src/main/java/com/example/healthyeatingapp/MainActivity.kt
+++ app/src/main/java/com/example/healthyeatingapp/MainActivity.kt
@@ -7,7 +7,7 @@ import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.healthyeatingapp.ui.FruitScreen
 import com.example.healthyeatingapp.ui.theme.HealthyEatingAppTheme
 import com.example.healthyeatingapp.viewmodel.FruitViewModel
-
+// Entry point of the application that loads the main UI
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
